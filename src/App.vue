f0b1335a3e1ba42f3dad246da46cc298f0084e40
GuiDevloper <guicorrea384@gmail.com>
2018-09-03 05:40:26 -0300

    App comp. contato importado

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <contact/>
+  </div>
+</template>
+
+<script>
+import contact from "@/components/Contact.vue";
+
+export default {
+  components: {
+    contact
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  min-height: 100vh;
+  background-color: gray;
+  font-family: 'Raleway', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  border-radius: 3px;
+}
+</style>
