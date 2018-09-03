1e012cddaa0d994264857f7083624e6e2daf8418
GuiDevloper <guicorrea384@gmail.com>
2018-09-03 06:03:10 -0300

    Contato comp./envio de email

---
+++ src/components/Contact.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="contact" class="c-1o1">
+    <div class="email c-1o2">
+      <form>
+        <fieldset>
+          <legend>Email</legend>
+          <input name=""/>
+          <input name=""/>
+          <textarea name=""/>
+        </fieldset>
+      </form>
+    </div>
+    <div class="mid"/>
+    <div class="social c-1o2">
+
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: "contact"
+};
+</script>
+
+<style lang="scss" scoped>
+#contact {
+  min-height: 60vh;
+  background-color: white;
+  .email,
+  .social {
+    height: 100%;
+    float: left;
+    padding: 5vh;
+    padding-top: 10%;
+  }
+}
+</style>
