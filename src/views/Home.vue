6d5f7f3992c7d4dc01a9c5ae37a052a91b2a023d
GuiDevloper <guicorrea384@gmail.com>
2018-09-03 06:08:08 -0300

    View com Hello e navbar

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Hello/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hello from '@/components/Hello.vue';
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Hello
+  }
+}
+</script>
