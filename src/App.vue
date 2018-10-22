3725fade62d50624b632f2b00684d6e1c4ad2579
GuiDevloper <guicorrea384@gmail.com>
2018-10-21 23:18:36 -0300

    Up. Backs, font and footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <div class="bg-l">
-      <img src="./assets/SVG/back-left.svg">
+      <div></div>
     </div>
     <div class="bg-r">
-      <img src="./assets/SVG/back-right.svg">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1409 2749"><path d="M110 10c-1-1-10 22-19 47a1336 1336 0 0 0-52 168 648 648 0 0 0-9 366c14 59 27 80 44 128 32 93 38 169 43 243 7 94 12 167-11 260-19 77-45 114-62 220-3 23-9 59-10 105-4 229 91 267 77 527-6 117-30 127-37 246-4 63 12 218 64 422l1264-3-3-2731-1289 2z" class="cls-2"/><path d="M104 4a1600 1600 0 0 0-21 52l-21 58c-17 53-31 107-43 161a642 642 0 0 0 36 416 741 741 0 0 1 43 159c8 50 11 102 14 153 4 55 6 112-3 167-7 45-21 89-36 132s-28 87-35 133c-14 88-16 177 3 265 15 68 41 133 54 202 16 91 15 188-6 278-16 69-26 136-21 207 6 91 23 181 42 270l21 87a8 8 0 0 0 7 5l986-2 278-1a8 8 0 0 0 7-7v-617l-1-703-1-668V83 11 7a8 8 0 0 0-8-7h-128L964 1 592 2H112c-1 0-4 0 0 0-10-2-14 13-4 15 11 2 24 0 36 0h401l372-1 323-1h157a10 10 0 0 1 2 0l-7-7v670l1 654 1 705v702l8-8-986 2-278 1 7 6c-25-100-46-202-57-305-8-70-9-139 6-209 7-35 17-70 21-106a795 795 0 0 0 3-156c-6-72-27-141-47-210a680 680 0 0 1-2-390c10-38 25-75 37-113a582 582 0 0 0 24-154c2-53-3-107-7-160-4-49-9-98-20-146-12-49-30-94-47-140-34-90-48-189-38-284 6-54 20-108 33-160A1344 1344 0 0 1 93 70l12-30a1154 1154 0 0 1 10-25c6-7-4-18-11-11z" class="cls-2"/></svg>
     </div>
     <router-view/>
     <contact/>
     <footer>
-      <p>Construído com amor por GuiDevloper</p>
+      <p class="p1">Construído com</p>
+      <p class="p2"> amor </p>
+      <p class="p3">por GuiDevloper</p>
     </footer>
   </div>
 </template>
@@ -25,8 +27,10 @@ export default {
 </script>
 
 <style lang="scss">
+$black: #20272f;
+$white: #e5e5e5;
 body {
-  background: #171717;
+  background: $white;
   margin: 0;
   &::-webkit-scrollbar {
     width: 4px;
@@ -36,7 +40,7 @@ body {
     border-radius: 10px;
   }
   &::-webkit-scrollbar-track {
-    background-color: #171717;
+    background-color: $black;
   }
 }
 .bg-l,
@@ -46,18 +50,32 @@ body {
   z-index: -5;
   margin-top: -2px;
   margin-bottom: -5px;
-  width: 65%;
-  img {
+  width: 60%;
+  img, svg {
     height: 142vh;
+    flex: 1;
+    min-height: 700px;
   }
 }
 .bg-l {
-  background: linear-gradient(to right, #e5e5e5 50%, transparent 50%);
+  background: linear-gradient(to right, $white 50%, transparent 50%);
+  display: none;
+  div {
+    background: url("./assets/SVG/back-left.svg");
+  }
 }
 .bg-r {
   z-index: 1;
-  margin-left: 40%;
-  background: linear-gradient(to left, #171717 50%, transparent 50%);
+  margin-left: 47vw;
+  background: linear-gradient(to left, $black 70%, transparent 50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: flex-start;
+  svg {
+    .cls-2{fill:$black}
+  }
 }
 * {
   -webkit-box-sizing: border-box;
@@ -77,33 +95,57 @@ span {
 p {
   margin: 0;
 }
+input,
+textarea, legend, a {
+  font-size: 1rem;
+  font-family: "Raleway";
+  text-decoration: none;
+}
+.lik {
+  color: #e5e5e5;
+  &:hover {
+    color: #20272f;
+  }
+}
 #app {
   min-height: 100vh;
   font-family: "Raleway", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: white;
-}
-a {
-  color: #ffeb3b;
-  background: #761f1f;
-  padding: 0 1px;
+  color: $white;
+  font-weight: normal;
 }
 footer {
-  width: 100%;
-  float: left;
-  z-index: 3;
+  margin-top: -10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
   p {
-    text-align: center;
-    width: fit-content;
-    background: linear-gradient(to bottom, rgba(0, 0, 0, 0.4) 62%, #973333 0);
-    margin: auto;
+    background: linear-gradient(to bottom, transparent 62%, #973333 0);
+    z-index: 999;
+    font-size: large;
+  }
+  .p1 {
+    color: #000000;
+    border-radius: 0 5px;
+  }
+  .p2 {
+    color: #F44336;
+    border-radius: 0;
+  }
+  .p3 {
+    border-radius: 5px 0;
   }
 }
 @media (max-width: 767px) {
   #app {
     font-size: 16px;
+  }
+}
+@media (max-width: 991px) {
+  .p1 {
+    margin-left: 5%;
   }
 }
 </style>
