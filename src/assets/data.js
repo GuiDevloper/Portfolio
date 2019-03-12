1f1fbae2c56066a2e9145333c05dfc2039e260e1
GuiDevloper <guicorrea384@gmail.com>
2019-03-11 22:49:29 -0300

    moved main data of page to file, v0.8.0

---
+++ src/assets/data.js
@@ -0,0 +1,82 @@
+const data = {
+  options1: [
+    [
+      [0, 'The Sea', false],
+      [1, 'The Inspiration', false],
+      [2, 'The Dreamity', false],
+      [3, 'The Portfolio', false]
+    ],
+    [
+      [0, `<-`],
+      [1, 'Ver no GitHub', 'https://github.com/guidevloper/'],
+      [2, 'Ver Online', 'https://guidevloper.github.io/']
+    ]
+  ],
+  descriptions: [
+    `Actually i work with diverse tools and
+    continually learn others, in ancient i has edit images in Photoshop
+    and create some films with After Effects. Now i am focused to craft
+    my criativity in Illustrator and too code my ideas using JS, SCSS,
+    PHP, MySQL and eventually Gulp.js to optimize my workflow between
+    compilations and rebuilds.`,
+    `Actually i work with diverse tools and
+    continually learn others, in ancient i has edit images in Photoshop
+    and create some films with After Effects.`,
+    `A missão é principalmente oferecer uma forma de pessoas compartilharem o que tem sonhado,
+      dormindo ou não.<br>
+      O sonhador cria sua conta e customizará seu perfil.<br>
+      Após isso, pode escrever seus sonhos em estilo de post, podendo adicionar imagem de fundo.<br>
+      Outros sonhadores ao lerem este sonho, podem demonstrar simples opiniões com um slider estilo:<br>
+      <code>Sonho Bem Imaginado -> Pode Ser Realizado</code>.<br>
+      Ou com comentários possivelmente complexos usando texto.`
+  ],
+  codes: [
+    [
+      ["cd1 red", "Guilherme"],
+      ["cd2", "Correia ou GuiDevloper"]
+    ],
+    [
+      ["cd3 grey", "desde pequeno se via transformando"]
+    ],
+    [
+      ["cd4 red", "coisas,"]
+    ],
+    [
+      ["cd5 blue", "desmontando brinquedos"],
+      ["cd6 green", "e os"],
+      ["cd7 red", "estudando"]
+    ],
+    [
+      ["cd8 blue", "percebia que"],
+      ["cd9 green", "eles tinham novos usos,"]
+    ],
+    [
+      ["cd10 red", "estimulando"]
+    ],
+    [
+      ["cd11 blue", "a criatividade"],
+      ["cd12 green", "que logo receberia"],
+      ["cd13 yellow", "um alvo"]
+    ],
+    [
+      ["cd16 grey", "ao se formar um técnico em informática."]
+    ],
+    [
+      ["cd17 red", "Desde"],
+      ["cd18", "então soube o caminho"],
+      ["cd19 yellow", "que seria necessário seguir"]
+    ],
+    [
+      ["cd20 blue", "a fim de expressar sua paixão criativa"]
+    ],
+    [
+      ["cd21 red", "por desenvolvimento"]
+    ]
+  ],
+  imgs: [
+    [1, "/img/TheSea.34953e45.jpg"], [2, "/img/TheSea2.f70b81b5.jpg"],
+    [1, "/img/Inspiration-d.f81d39da.jpg"], [2, "/img/Inspiration-w.170dfdd3.jpg"]
+  ]
+};
+
+export default data;
