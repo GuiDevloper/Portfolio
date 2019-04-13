fb7e9b2d51759468b097b9f128eed1d97286828b
GuiDevloper <guicorrea384@gmail.com>
2019-04-13 20:35:45 -0300

    updated lists of projects & imgs

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,14 +1,19 @@
 const data = {
   options1: [
     [
-      ['The Sea', 'The Inspiration', 'The Dreamity', 'The Portfolio'],
-      ['Guilherme Strange', 'The Panther', 'The Sea Ilustrations']
+      ['The Sea', 'The Inspiration', 'The Dreamity', 'The Portfolio', 'MedWay', 'Minimalista', 'UniversoVersos'],
+      ['Strange\'s Effect', 'The Panther', 'The Sea Ilustrations'],
+      ['Tradução no vuejs.org', 'What You Made Today?']
     ],
     [
-      [`<-`, 'Ver no GitHub', 'Ver Online']
+      ['<-', 'Ver no GitHub', 'Ver Online']
     ],
     [
-      [null, 'https://github.com/guidevloper/', 'https://guidevloper.github.io/']
+      [null, 'https://github.com/guidevloper/', '.netlify.com/'],
+      [null, 'https://github.com/vuejs-br/br.vuejs.org/pulls?q=author%3AGuiDevloper', 'https://br.vuejs.org/v2/cookbook/unit-testing-vue-components'],
+      [null, 'https://github.com/guidevloper/minimalista', 'https://www.npmjs.com/package/minimalista'],
+      [null, 'https://github.com/GuiDevloper/what-have-you-made-today', null],
+      [null, null, null]
     ]
   ],
   descriptions: [
@@ -29,6 +34,11 @@ const data = {
       Outros sonhadores ao lerem este sonho, podem demonstrar simples opiniões com um slider estilo:<br>
       <code>Sonho Bem Imaginado -> Pode Ser Realizado</code>.<br>
       Ou com comentários possivelmente complexos usando texto.`
+    ],
+    [
+      `Actually i work with diverse tools and continually learn others,`,
+      `Missão é principalmente oferecer uma forma de pessoas compartilharem o que tem sonhado,<br>`,
+      `<code>Sonho Bem Imaginado -> Pode Ser Realizado</code>.`
     ],
     [
       `Actually i work with diverse tools and continually learn others,`,
@@ -81,12 +91,22 @@ const data = {
   ],
   imgs: [
     [
-      [1, "/img/TheSea.34953e45.jpg"], [2, "/img/TheSea2.f70b81b5.jpg"],
-      [1, "/img/Inspiration-d.f81d39da.jpg"], [2, "/img/Inspiration-w.170dfdd3.jpg"]
+      [1, "/img/TheSea1.e121d4c7.jpg"], [2, "/img/TheSea2.b2f0d16a.jpg"],
+      [1, "/img/inspiration1.ab8299c5.jpg"], [2, "/img/inspiration2.74a3fd39.jpg"],
+      [1, "/img/dreamity1.14a762b9.png"], [2, "/img/dreamity2.6bd42d72.png"],
+      [1, "/img/shapes.530c8e2f.jpg"],
+      [1, "/img/medway1.8a2440bd.jpg"], [2, "/img/medway2.b674a0e4.jpg"],
+      [1, "/img/minimalista1.de5a97f2.jpg"],
+      [1, "/img/universe1.280e09b1.png"], [2, "/img/universe2.43876a53.png"]
     ],
     [
-      [1, "/img/Inspiration-d.f81d39da.jpg"], [2, "/img/TheSea2.f70b81b5.jpg"],
-      [1, "/img/TheSea.34953e45.jpg"], [2, "/img/Inspiration-w.170dfdd3.jpg"]
+      [1, "/img/strange1.1c46d9f3.jpg"],
+      [1, "/img/ThePanther1.b3b83f53.jpg"],
+      [1, "/img/Illustrations.5ee7857c.jpg"]
+    ],
+    [
+      [1, "/img/vueDoc1.d9cfe047.jpg"],
+      [1, "/img/whymt1.cece6f83.png"], [2, "/img/whymt2.bf1fbc85.png"]
     ]
   ]
 };
